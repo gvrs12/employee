c93b5eb396b65e7e64be42c086ae7dd288bbb393
gvrs12 <40173337+gvrs12@users.noreply.github.com>
2020-10-16 18:02:43 +0530

    Update publish.sh

--- ci/publish.sh
+++ ci/publish.sh
@@ -6,4 +6,5 @@ cd dev-src
 mvn clean -DskipTests package
 #cf login -a https://api.sys.cfnp-1.gaig.com -o developers -s sguddeti -u sguddeti -p Venkat@boduppal2009 --skip-ssl-validation
 cf login -a "${pcf_api_endpoint_uri}" -o "${pcf_org_name}" -s "${pcf_space_name}" -u "${pcf_account_username}" -p "${pcf_account_password}" --skip-ssl-validation
+cf target "${pcf_org_name}" -s "${pcf_space_name}"
 cf push helloworld-sguddeti
